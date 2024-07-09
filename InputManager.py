c58f147f7bb1b77b8aebb0cc76e7c48eea5e5c33
Dylan Lupon <dylan1lupon@gmail.com>
2024-07-09 23:01:28 +0200

    added Mouse Movements
    Main can now randomly move the mmouse

--- InputManager.py
+++ InputManager.py
@@ -1,34 +1,34 @@
 # autor : LUPON Dylan
 # date ; 07 / 09 / 2024
 
+import random
 import math
+import time
 import pyautogui as pauto
 
 class InputManager:
 
-    class Mouse:
+    # MOUSE PROPERTIES
+    DEFAULT_SPEED : float = 1000
+    MOUSE_LEFT_CLICK : str = "left"
+    MOUSE_MIDDLE_CLICK : str = "middle"
+    MOUSE_RIGHT_CLICK : str = "right"
 
-        DEFAULT_SPEED : float = 10
-        MOUSE_LEFT_CLICK : str = "left"
-        MOUSE_MIDDLE_CLICK : str = "middle"
-        MOUSE_RIGHT_CLICK : str = "right"
+    ################################### MOUSE
 
-        # MOVEMENTS
-        def MouseMove(pX : float,  pY : float, pSpeed : float = DEFAULT_SPEED):
-            lDistance : float = GetDistance(pauto.position(), pauto.Point(pX, pY))
-            lDuration : float = lDistance / pSpeed
-            pauto.moveTo(pX, pY, duration = lDuration)
+    # MOVEMENTS
+    def MouseMove(pX : float,  pY : float, pSpeed : float = DEFAULT_SPEED) -> float:
+        lDistance : float = InputManager.GetDistance(pauto.position(), pauto.Point(pX, pY))
+        lDuration : float = lDistance / pSpeed
+        pauto.moveTo(pX, pY, duration = lDuration)
+        return lDuration
 
-        def MouseMoveInstant(pX : float, pY : float) : pauto.moveTo(pX, pY, duration= 0)
+    def MouseMoveInstant(pX : float, pY : float) : pauto.moveTo(pX, pY, duration= 0)
 
-        # CLICK
-        def MouseClick(pClickCount : int = 1, pClickType : string = MOUSE_LEFT_CLICK):
-            for lClickIndex in range(pClickCount):
-                pauto.click(button= pClickType)
-
-    class KeyBoard:
-
-        pass
+    # CLICK
+    def MouseClick(pClickCount : int = 1, pClickType : str = MOUSE_LEFT_CLICK):
+        for lClickIndex in range(pClickCount):
+            pauto.click(button= pClickType)
 
     ################################### UTILITIES
 
--- Main.py
+++ Main.py
@@ -1,2 +1,10 @@
 # autor : LUPON Dylan
 # date ; 07 / 09 / 2024
+
+import time
+import random
+
+from InputManager import InputManager as IP
+
+for i in range(25):
+    time.sleep(IP.MouseMove(random.randint(0, 1920), random.randint(0, 1080)))
